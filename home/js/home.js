bcf8c2fbf78171f1f83d3b7fc07b9daf02b82155
seonyo <jsy6535@naver.com>
2023-10-21 03:37:05 +0900

    ✨[Feat] value 숨기기

--- home/js/home.js
+++ home/js/home.js
@@ -4,6 +4,7 @@ let userid;
 let title;
 let category;
 let q2;
+let PK;
 
 let mainLabel = document.getElementById("main-label");
 let ideaTitle = document.getElementById("idea-title");
@@ -11,18 +12,27 @@ let ideaCategory = document.getElementById("idea-category");
 let ideaQ2 = document.getElementById("q2");
 
 axios.get(`http://192.168.11.206:3000/idea/${id}`)
-  .then((response) => {
-    console.log(response);
-    userid = response.data.result[0].name;
-    title = response.data.results[0].title;
-    category = response.data.results[0].q1;
-    q2 = response.data.results[0].q2;
+    .then((response) => {
+        console.log(response);
+        userid = response.data.result[0].name;
+        title = response.data.results[0].title;
+        category = response.data.results[0].q1;
+        q2 = response.data.results[0].q2;
+        PK = response.data.results[0].id;
 
-    mainLabel.innerHTML = userid + "님의 아이디어 발자국";
-    ideaTitle.innerHTML = title;
-    ideaCategory.innerHTML = category;
-    ideaQ2.innerHTML = q2;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+        mainLabel.innerHTML = userid + "님의 아이디어 발자국";
+        ideaTitle.innerHTML = title;
+        ideaCategory.innerHTML = category;
+        ideaQ2.innerHTML = q2;
+        document.querySelector(".idea-header").value = PK;
+
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+document.querySelector(".idea-header").addEventListener("click", function (event) {
+    const value = event.currentTarget.value;
+
+    
+});
