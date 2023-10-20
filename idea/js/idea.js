14ca421e841adc6d7dd664229505412e66d14bcf
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-21 05:04:47 +0900

    [Fix] 데이터 수정

--- idea/js/idea.js
+++ idea/js/idea.js
@@ -2,13 +2,11 @@ let id = new URL(location.href).searchParams.get('value');
 const ideaTitle = document.getElementsByClassName('idea-title')[0];
 const pContentList = document.getElementsByClassName('p-content');
 
-axios.get(`http://192.168.11.206:3000/idea/3`)
+axios.get(`http://192.168.11.206:3000/idea/2`)
     .then((response) => {
-        console.log(response.data)
-        console.log(response.data.result);
-        var data = response.data.idea;
+        let data = response.data.idea[0];
+        console.log(data);
         ideaTitle.innerHTML = data.title;
-
         pContentList[0] = data.q2;
         pContentList[1] = data.q3;
         pContentList[2] = data.q4;
