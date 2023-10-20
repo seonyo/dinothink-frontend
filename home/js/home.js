0a055fb63bfc0ea76ec78fe7593e63187e4674a7
seonyo <jsy6535@naver.com>
2023-10-21 04:23:35 +0900

    ✨[Feat] home화면 최근 순 글부터 보여주기

--- home/js/home.js
+++ home/js/home.js
@@ -13,18 +13,42 @@ let ideaQ2 = document.getElementById("q2");
 
 axios.get(`http://192.168.11.206:3000/idea/${id}`)
     .then((response) => {
-        console.log(response);
+        let size = response.data.results.length;
+
         userid = response.data.result[0].name;
-        title = response.data.results[0].title;
-        category = response.data.results[0].q1;
-        q2 = response.data.results[0].q2;
-        PK = response.data.results[0].id;
+        title = response.data.results[size - 1].title;
+        category = response.data.results[size - 1].q1;
+        q2 = response.data.results[size - 1].q2;
+        PK = response.data.results[size - 1].id;
 
         mainLabel.innerHTML = userid + "님의 아이디어 발자국";
         ideaTitle.innerHTML = title;
         ideaCategory.innerHTML = category;
         ideaQ2.innerHTML = q2;
         document.querySelector(".idea-header").value = PK;
+
+        let ideasContainer = document.getElementById("ideas");
+
+        if (size > 1) {
+            for (let i = size - 1; i >= 0; i--) {
+                const idea = response.data.results[i];
+                const ideaContainer = document.createElement("div");
+                ideaContainer.classList.add("idea-container");
+
+                const ideaTitle = document.createElement("div");
+                ideaTitle.classList.add("idea-title");
+                ideaTitle.innerText = idea.title;
+
+                const ideaCategory = document.createElement("div");
+                ideaCategory.classList.add("idea-category");
+                ideaCategory.innerText = idea.q1;
+
+                ideaContainer.appendChild(ideaTitle);
+                ideaContainer.appendChild(ideaCategory);
+
+                ideasContainer.appendChild(ideaContainer);
+            }
+        }
 
     })
     .catch((error) => {
@@ -34,5 +58,5 @@ axios.get(`http://192.168.11.206:3000/idea/${id}`)
 document.querySelector(".idea-header").addEventListener("click", function (event) {
     const value = event.currentTarget.value;
 
-    
 });
+
