3d2a46d2683d503c0205ca2eabe83b0e3a5ce363
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-21 04:37:01 +0900

    [Feat] 글 목록 가져오기

--- progress/js/progress.js
+++ progress/js/progress.js
@@ -1,5 +1,31 @@
 const addIdeaContainer = document.getElementsByClassName('add-idea-container')[0];
+const main = document.getElementById('main');
+const ideaContainer = document.getElementsByClassName('idea-container');
 
 addIdeaContainer.onclick = () => {
     window.open('/sketch', '_top');
 };
+
+let id = new URL(location.href).searchParams.get('id');
+
+
+axios.get(`http://192.168.11.206:3000/idea/${id}`)
+    .then((response) => {
+        let data = response.data.results;
+        console.log(data);
+        data.forEach(element => {
+            main.innerHTML += `
+            <div class="idea-container">
+                <div class="idea-title">${element.title}</div>
+                <div class="idea-progress">
+                    <img src="../images/progress${element.checking}.png" alt="">
+                </div>
+            </div>
+            `;
+        });
+        
+
+    })
+    .catch((error) => {
+        console.error(error);
+    });
