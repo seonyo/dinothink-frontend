400cbbcf27474e44269ca624eb2dc20c912d125b
hyeg0121 <w2218@e-mirim.hs.kr>
2023-10-21 05:16:31 +0900

    [Feat] 데이터의 값으로 요소에 값 대입

--- idea/js/idea.js
+++ idea/js/idea.js
@@ -2,16 +2,16 @@ let id = new URL(location.href).searchParams.get('value');
 const ideaTitle = document.getElementsByClassName('idea-title')[0];
 const pContentList = document.getElementsByClassName('p-content');
 
-axios.get(`http://192.168.11.206:3000/idea/2`)
+axios.get(`http://192.168.11.206:3000/idea/${id}`)
     .then((response) => {
         let data = response.data.idea[0];
         console.log(data);
         ideaTitle.innerHTML = data.title;
-        pContentList[0] = data.q2;
-        pContentList[1] = data.q3;
-        pContentList[2] = data.q4;
-        pContentList[3] = data.q5;
-        pContentList[4] = data.q6;
+        pContentList[0].innerHTML = data.q2;
+        pContentList[1].innerHTML = data.q3;
+        pContentList[2].innerHTML = data.q4;
+        pContentList[3].innerHTML = data.q5;
+        pContentList[4].innerHTML = data.q6;
     })
     .catch((error) => {
         console.error(error);
